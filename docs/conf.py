efce608cc4f381161be7b113d25180e92fdee58d
pradeep <pradeepsn606@gmail.com>
2023-04-03 13:02:48 +0530

    Adds depreciation warning

--- docs/conf.py
+++ docs/conf.py
@@ -62,6 +62,9 @@ warnings.filterwarnings("ignore", message='The sunpy.database module is no longe
 warnings.filterwarnings("error", category=SunpyPendingDeprecationWarning)
 warnings.filterwarnings("error", category=MatplotlibDeprecationWarning)
 warnings.filterwarnings("error", category=AstropyDeprecationWarning)
+warnings.filterwarnings("ignore", message="The `sunpy.io.cdf` module is deprecated",category=SunpyDeprecationWarning,stacklevel=0)
+warnings.filterwarnings("ignore", message="The `sunpy.io.jp2` module is deprecated",category=SunpyDeprecationWarning,stacklevel=0)
+warnings.filterwarnings("ignore", message="The `sunpy.io.file_tools` module is deprecated",category=SunpyDeprecationWarning,stacklevel=0)
 
 # -- SunPy Sample Data and Config ----------------------------------------------
 # We set the logger to debug so that we can see any sample data download errors
---
+++ sunpy/io/jp2.py
@@ -0,0 +1,9 @@
+from sunpy.util.exceptions import warn_deprecated
+from . import _jp2
+from ._jp2 import *  
+
+__doc__ = _jp2.__doc__
+__all__ = _jp2.__all__
+
+warn_deprecated("The `sunpy.io.fits` module is deprecated, as it was designed "
+                "for internal use.")

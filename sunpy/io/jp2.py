e199d24b2c6af2c883c5f4542c4e2d2973bbad9c
pradeep <pradeepsn606@gmail.com>
2023-04-03 16:26:40 +0530

    doc fixes

--- sunpy/io/jp2.py
+++ sunpy/io/jp2.py
@@ -1,6 +1,6 @@
 from sunpy.util.exceptions import warn_deprecated
 from . import _jp2
-from ._jp2 import read,get_header,header_to_xml
+from _jp2 import read
 __doc__ = _jp2.__doc__
 __all__ = _jp2.__all__
 
